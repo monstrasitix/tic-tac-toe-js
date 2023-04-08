c4b0d6361fd4cb234b4c93e4eda0f79a431fc792
monstrasitix <injectedinferno@gmail.com>
2023-04-08 20:37:09 +0100

    Clean up

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,9 @@
 import { drawBackground, drawGrid, drawO, drawX } from './draw';
 import { createCanvas, draw, mount, resize } from './context';
 import { CELL_COUNT } from './config';
+import { randInteger, randBool } from './random';
+
+const symbols: Array<{ type: 'X' | 'O', x: number, y: number }> = [];
 
 const ctx = createCanvas();
 mount(ctx, document.body);
@@ -9,6 +12,13 @@ resize(ctx);
 const initialRender = (buffer: CanvasRenderingContext2D) => {
     drawBackground(buffer);
     drawGrid(buffer);
+
+    for (const symbol of symbols) {
+        (symbol.type === 'O'
+            ? drawO
+            : drawX
+        )(symbol.x, symbol.y)(buffer);
+    }
 };
 
 window.addEventListener('resize', () => {
@@ -17,33 +27,27 @@ window.addEventListener('resize', () => {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    draw(ctx, initialRender);
-
-    const rand = (max: number, min: number = 0) => (
-        Math.floor(Math.random() * (max - min)) + min
-    );
-
     const cords: [number, number][] = [];
-
-    const randBool = () => Math.random() >= 0.5;
 
     const constrain = CELL_COUNT * Math.floor(CELL_COUNT * 0.7);
 
     for (let index = 0; index < constrain; index++) {
-        let x = rand(CELL_COUNT);
-        let y = rand(CELL_COUNT);
+        let x = randInteger(CELL_COUNT);
+        let y = randInteger(CELL_COUNT);
 
         while (cords.some(a => a[0] === x && a[1] === y)) {
-            x = rand(CELL_COUNT);
-            y = rand(CELL_COUNT);
+            x = randInteger(CELL_COUNT);
+            y = randInteger(CELL_COUNT);
         }
 
         cords.push([x, y]);
 
-        if (randBool()) {
-            draw(ctx, drawX(x, y));        
-        } else {
-            draw(ctx, drawO(x, y));
-        }
+        symbols.push({
+            type: randBool() ? 'X' : 'O',
+            x,
+            y,
+        });
     }
+
+    draw(ctx, initialRender);
 });
---
+++ source/random.ts
@@ -0,0 +1,5 @@
+export const randInteger = (max: number, min: number = 0) => (
+    Math.floor(Math.random() * (max - min)) + min
+);
+
+export const randBool = () => Math.random() >= 0.5;
